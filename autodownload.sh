971a55d7e979e68d8840a911ac5bfae55efa7095
Thomas Jones <tjone270@me.com>
2015-10-04 14:06:16 +1000

    Made all script echo text blue to improve readability as the script runs.

--- autodownload.sh
+++ autodownload.sh
@@ -5,17 +5,20 @@
 
 qldsPath="/home/qlserver/steamcmd/steamapps/common/qlds/"
 
+
+BLUE="\033[0;34m"
+NC="\033[0m"
 workshopIDs=`cat $qldsPath/baseq3/workshop.txt | grep -v '#' | sed '/^[ \t]*$/d'`
 numOfIDs=`echo "$workshopIDs" | wc -l`
 counter=0
 while [ $counter -le $numOfIDs ]; do
 	currentID=`echo $workshopIDs | awk '{ print $1 }'`
 	workshopIDs=`echo $workshopIDs | cut -d ' ' -f2-`
-	echo -e "\nDownloading item $currentID from Steam...\n"
+	echo -e "\n\n${BLUE}Downloading item $currentID from Steam...${NC}\n"
 	~/steamcmd/steamcmd.sh +login anonymous +workshop_download_item 344320 $currentID +quit
 	((counter++))
 done
-echo -e "\n\nRemoving old workshop data and moving new items into place..."
+echo -e "\n\n${BLUE}Removing old workshop data and moving new items into place${NC}..."
 rm -r ~/steamcmd/steamapps/common/qlds/steamapps/workshop
 mv ~/steamcmd/steamapps/workshop/ ~/steamcmd/steamapps/common/qlds/steamapps/workshop
 echo Done.
