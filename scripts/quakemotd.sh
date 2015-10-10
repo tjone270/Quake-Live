0c62169059c67846959bc6c741b89d6385c2fdf4
Thomas Jones <thomas@tomtecsolutions.com>
2015-10-10 22:13:38 +1000

    moved delay to beginning to allow time for autodownload.sh to finish. added header.

--- scripts/quakemotd.sh
+++ scripts/quakemotd.sh
@@ -11,10 +11,14 @@ export qUpdateHighestRconPort=28970
 export qBaseURL="https://raw.githubusercontent.com/tjone270/QuakeLiveDS_Scripts/master"
 export qDelayBetweenMOTDbroadcast="600"  # in seconds
 
+echo "========== QuakeMOTD.sh has started. =========="
+echo "========== $(date) =========="
+
 # Broadcast MOTD to every server hosted locally.
 counter="$qUpdateLowestRconPort"
 while true
 do
+    sleep $qDelayBetweenMOTDbroadcast
     # Download latest MOTD from GitHub.
     curl -s $qBaseURL/motd.txt > remoteConfig-motd.txt; dos2unix --quiet remoteConfig-motd.txt
     export qMOTDcontent=$(<remoteConfig-motd.txt)
@@ -27,7 +31,6 @@ do
 
     # Reset counter.
     export counter="$qUpdateLowestRconPort"
-    sleep $qDelayBetweenMOTDbroadcast
 done
 
 exit 0
