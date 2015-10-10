0ce5cc99a3a8ced4349da0750094b7be5be61eb6
Thomas Jones <thomas@tomtecsolutions.com>
2015-10-10 22:12:48 +1000

    added an item count, so the user can see progress.

--- scripts/autodownload.sh
+++ scripts/autodownload.sh
@@ -3,6 +3,8 @@
 # created by Thomas Jones on 03/10/15.
 # purger@tomtecsolutions.com
 
+echo "========== AutoDownload.sh has started. =========="
+echo "========== $(date) =========="
 
 BLUE="\033[0;34m"
 NC="\033[0m"
@@ -12,8 +14,8 @@ counter=0
 while [ $counter -lt $numOfIDs ]; do
 	currentID=`echo $workshopIDs | awk '{ print $1 }'`
 	workshopIDs=`echo $workshopIDs | cut -d ' ' -f2-`
-	echo -e "\n\n${BLUE}Downloading item $currentID from Steam...${NC}\n"
-	~/steamcmd/steamcmd.sh +login anonymous +workshop_download_item 344320 $currentID +quit
+	echo -e "\n\n${BLUE}Downloading item $currentID from Steam... ($counter/$numOfIDs)${NC}\n"
+	~/steamcmd/steamcmd.sh +login anonymous +workshop_download_item 344320 $currentID +quit > /dev/null
 	((counter++))
 done
 echo -e "\n\n${BLUE}Removing old workshop data and moving new items into place...${NC}"
