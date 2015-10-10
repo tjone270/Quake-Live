2fca6fd5bb932bde36106de2eed2ab4d7acdbe24
Thomas Jones <thomas@tomtecsolutions.com>
2015-10-10 17:56:36 +1000

    enables periodic MOTD broadcast functionality. Designed to be a 'bg'ed process, disowned by the current tty.

---
+++ scripts/quakemotd.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+# quakemotd.sh - quake live message of the day server broadcaster.
+# created by Thomas Jones on 10/10/15.
+# purger@tomtecsolutions.com
+
+# Defining variables:
+export qRconPassword=$(<localConfig-rconPassword.txt)
+export qMOTDcontent=$(<remoteConfig-motd.txt)
+export qUpdateLowestRconPort=28960
+export qUpdateHighestRconPort=28970
+export qBaseURL="https://raw.githubusercontent.com/tjone270/QuakeLiveDS_Scripts/master"
+export qDelayBetweenMOTDbroadcast="0"  # in seconds
+
+# Broadcast MOTD to every server hosted locally.
+counter="$qUpdateLowestRconPort"
+while true
+do
+    while [ $counter -le $qUpdateHighestRconPort ]
+    do
+        echo "Broadcasting MOTD to port $counter"
+        ~/steamcmd/steamapps/common/qlds/rcon.py --host tcp://127.0.0.1:$counter --password "$qRconPassword" --command "say \"$qMOTDcontent\""
+        ((counter++))
+    done
+
+    # Download latest MOTD from GitHub.
+    rm remoteConfig-motd.txt; curl $qBaseURL/motd.txt > remoteConfig-motd.txt; dos2unix remoteConfig-motd.txt
+
+    sleep $qDelayBetweenMOTDbroadcast
+done
+
+exit 0
