890d637061a685154b771a854ef75d29ef7fea38
Thomas Jones <thomas@tomtecsolutions.com>
2015-10-15 01:48:02 +1000

    Made MOTD multi-line (it'll work this time, I promise!)

--- scripts/quakemotd.sh
+++ scripts/quakemotd.sh
@@ -4,7 +4,7 @@
 # purger@tomtecsolutions.com
 
 # Defining variables:
-export qMOTDcontent=$(<remoteConfig-motd.txt)
+export qMOTDcontentFileName="remoteConfig-motd.txt"
 export qRconPassword=$(<localConfig-rconPassword.txt)
 export qUpdateLowestRconPort=28960
 export qUpdateHighestRconPort=28970
@@ -18,27 +18,28 @@ echo "========= $(date) ========="
 counter="$qUpdateLowestRconPort"
 while true
 do
-    sleep $qDelayBetweenMOTDbroadcast
-    # Download latest MOTD from GitHub.
-    curl -s $qBaseURL/motd.txt > remoteConfig-motd.txt; dos2unix --quiet remoteConfig-motd.txt
-    export qMOTDcontent=$(<remoteConfig-motd.txt)
-    while [ $counter -le $qUpdateHighestRconPort ]
-    do
-        #echo "Broadcasting MOTD to port $counter"
-        python steamcmd/steamapps/common/qlds/rcon.py --host tcp://127.0.0.1:$counter --password "$qRconPassword" --command "say '^5Message of the Day:^7'" > /dev/null
-        counter2=3 # Skip line 1 and 2 to allow me to put instructions in.
-        counted2=`echo $qMOTDcontent | wc -l`
-        while [ $counter2 -le $counted2 ]
-        do
-            qBroadcastLine=`echo $qMOTDcontent | sed "${counter2}q;d"` # | grep -v '#'
-            python steamcmd/steamapps/common/qlds/rcon.py --host tcp://127.0.0.1:$counter --password "$qRconPassword" --command "say \"^7$qBroadcastLine\"" > /dev/null
-            ((counter2++))
-        done
-        ((counter++))
-    done
+sleep $qDelayBetweenMOTDbroadcast
+# Download latest MOTD from GitHub.
+curl -s $qBaseURL/motd.txt > remoteConfig-motd.txt; dos2unix --quiet remoteConfig-motd.txt
+while [ $counter -le $qUpdateHighestRconPort ]
+do
+echo "Broadcasting MOTD to port $counter"
+python steamcmd/steamapps/common/qlds/rcon.py --host tcp://127.0.0.1:$counter --password "$qRconPassword" --command "say ^5Message of the Day:^7" # > /dev/null
+counter2=3 # Skip line 1 and 2 to allow me to put instructions in.
+counted2=`cat $HOME/$qMOTDcontentFileName | wc -l`
+counted2=`expr $counted2 + 1`
+while [ $counter2 -le $counted2 ]
+do
+echo Line $counter2 of $counted2.
+qBroadcastLine=`cat $HOME/$qMOTDcontentFileName | sed "${counter2}q;d"` # | grep -v '#'
+python steamcmd/steamapps/common/qlds/rcon.py --host tcp://127.0.0.1:$counter --password "$qRconPassword" --command "say \"^7$qBroadcastLine\"" # > /dev/null
+((counter2++))
+done
+((counter++))
+done
 
-    # Reset counter.
-    export counter="$qUpdateLowestRconPort"
+# Reset counter.
+export counter="$qUpdateLowestRconPort"
 done
 
 exit 0
