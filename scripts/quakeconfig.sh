bcc2e7f2f26ef92ae5873b77f3f1bb9b766530f1
Thomas Jones <thomas@tomtecsolutions.com>
2015-10-19 09:13:24 +1000

    commented unused variables

--- scripts/quakeconfig.sh
+++ scripts/quakeconfig.sh
@@ -9,13 +9,13 @@ echo "========= $(date) ========="
 # Variables used in multiple scripts:
 cd ~
 export qBaseURL="https://raw.githubusercontent.com/tjone270/QuakeLiveDS_Scripts/master"
-export qRconPassword=$(<localConfig-rconPassword.txt)
-export qSteamUsername=$(<localConfig-steamUsername.txt)
-export qSteamPassword=$(<localConfig-steamPassword.txt)
-export qServerLocation=$(<localConfig-serverLocation.txt)
-export qUpdateServerMessage="^4The Purgery^7 servers are going down ^4within a minute^7 for daily updating. They will be back in ^410 minutes^7."
-export qUpdateLowestRconPort=28960
-export qUpdateHighestRconPort=28970
+#export qRconPassword=$(<localConfig-rconPassword.txt)
+#export qSteamUsername=$(<localConfig-steamUsername.txt)
+#export qSteamPassword=$(<localConfig-steamPassword.txt)
+#export qServerLocation=$(<localConfig-serverLocation.txt)
+#export qUpdateServerMessage="^4The Purgery^7 servers are going down ^4within a minute^7 for daily updating. They will be back in ^410 minutes^7."
+#export qUpdateLowestRconPort=28960
+#export qUpdateHighestRconPort=28970
 export qPathToStartScript="~/steamcmd/steamapps/common/qlds/run_server_x64.sh"
 export qStartPathToBaseQ3="~/steamcmd/steamapps/common/qlds/baseq3"
 export qQLDSpath="~/steamcmd/steamapps/common/qlds"
