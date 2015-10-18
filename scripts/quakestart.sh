8633e19349479adb157991e690cfb7681e1801be
Thomas Jones <thomas@tomtecsolutions.com>
2015-10-18 23:56:59 +1000

    removed that extra stupid 'then'

--- scripts/quakestart.sh
+++ scripts/quakestart.sh
@@ -292,9 +292,7 @@ exec $qPathToStartScript \
     +set bot_enable 1 \
     +set bot_nochat 1 \
     +set fs_homepath ~/.quakelive/$gameport
-else
-then
-echo "This system is not intended to host MickZeroFive's server."
+else; echo "This system is not intended to host MickZeroFive's server."
 fi
 
 fi
