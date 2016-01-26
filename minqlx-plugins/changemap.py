46bdb8d7ff772cd902a5d150196fb83f03c093c6
Thomas Jones <thomas@tomtecsolutions.com>
2016-01-27 07:10:33 +1000

    add changemap.py, a plugin to change the map when no-one's connected

---
+++ minqlx-plugins/changemap.py
@@ -0,0 +1,28 @@
+# Created by Thomas Jones on 27/01/2016 - thomas@tomtecsolutions.com
+# changemap.py, a plugin for minqlx to change the map when no-one's connected.
+# This plugin is released to everyone, for any purpose. It comes with no warranty, no guarantee it works, it's released AS IS.
+# You can modify everything, except for lines 1-4 and the !tomtec_versions code. They're there to indicate I whacked this together originally. Please make it better :D
+
+"""
+    This plugin will change the map to the mapname set in qlx_defaultMapToChangeTo. Default: campgrounds
+"""
+
+import minqlx
+
+class changemap(minqlx.Plugin):
+    def __init__(self):
+        self.add_hook("player_disconnect", self.player_disconnect)
+
+        self.add_command("tomtec_versions", self.cmd_showversion)
+
+        self.set_cvar_once("qlx_defaultMapToChangeTo", "campgrounds")
+        
+        self.plugin_version = "1.0"
+
+    def player_disconnect(self, player, reason):
+        if len(teams["red"] + teams["blue"] + teams["spectator"]) == 0:
+            self.change_map(self.get_cvar("qlx_defaultMapToChangeTo"))
+        
+        
+    def cmd_showversion(self, player, msg, channel):
+        channel.reply("^4changemap.py^7 - version {}, created by Thomas Jones on 27/01/2016.".format(self.plugin_version))
