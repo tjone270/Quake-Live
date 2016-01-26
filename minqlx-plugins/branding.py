ac8b176cc8c7f05d8f042891948d3ea5064cb611
Thomas Jones <thomas@tomtecsolutions.com>
2016-01-27 04:59:32 +1000

    add a heap of extra functionality, check it out!

--- minqlx-plugins/branding.py
+++ minqlx-plugins/branding.py
@@ -4,15 +4,21 @@
 # You can modify everything, except for lines 1-4 and the !tomtec_versions code. They're there to indicate I whacked this together originally. Please make it better :D
 
 """
-Branding.py is a minqlx plugin that permits you to modify your map loading screen with your own information.
+Branding.py is a minqlx plugin that permits you to personalise your server with your own information.
 Simply put the plugin in the 'minqlx-plugins' folder, !load the plugin, and set these cvars:
 
-    qlx_serverBrandName
-    qlx_serverBrandTopField
-    qlx_serverBrandBottomField
+    qlx_serverBrandName                  - Where the map name usually appears, the text set in this cvar will appear instead.
+    qlx_serverBrandTopField              - Where the map author credit (line 1) appears, the text set in this cvar will appear after the credit.
+    qlx_serverBrandBottomField           - Where the map author credit (line 2) appears, the text set in this cvar will appear after the credit.
 
-    qlx_brandingPrependMapName (this cvar will put the map name before your qlx_serverBrandName. Default: 0)
-
+    qlx_connectMessage                   - When the player is at the awaiting challenge screen when they first connect to the server, text will appear here.
+    qlx_loadedMessage                    - When the player gets to the menu after connecting, and clicks Join or Spectate, they'll get centre print from this cvar.
+    qlx_countdownMessage                 - When the countdown begins, this text will appear mid-screen. (like the qlx_loadedMessage does)
+    qlx_endOfGameMessage                 - When the game finishes, it'll put the text in this cvar in the text box on the left.
+    
+    qlx_brandingPrependMapName           - This cvar will put the map name before your qlx_serverBrandName.                     Default: 0
+    qlx_rainbowBrandName                 - Make the entire map name (qlx_serverBrandName) appear in rainbow colouring.          Default: 0
+    
 Once set, change maps, and you'll see the map loading screen is changed.
 """
 
@@ -21,14 +27,23 @@ import minqlx
 class branding(minqlx.Plugin):
     def __init__(self):
         self.add_hook("new_game", self.brand_map)
+        self.add_hook("player_connect", self.player_connect)
+        self.add_hook("player_loaded", self.player_loaded)
+        self.add_hook("game_countdown", self.game_countdown)
+        self.add_hook("game_end", self.game_end)
+        
         self.add_command("tomtec_versions", self.cmd_showversion)
+
         self.set_cvar_once("qlx_serverBrandName", "Branding isn't set up!")
         self.set_cvar_once("qlx_serverBrandTopField", "Set these cvars: 'qlx_serverBrandName', 'qlx_serverBrandTopField', 'qlx_serverBrandBottomField'.")
         self.set_cvar_once("qlx_serverBrandBottomField", "Tip: ^1C^2o^3l^4o^5u^6r ^1v^2a^3l^4u^5e^6s ^1a^2r^3e ^1s^2u^3p^4p^5o^6r^1t^2e^3d^4.")
 
         self.set_cvar_once("qlx_brandingPrependMapName", "0")
+        self.set_cvar_once("qlx_rainbowBrandName", "0")
         
-        self.plugin_version = "1.5"
+        self.plugin_version = "1.6"
+
+        self.playerConnectedYetList = []
         
     def brand_map(self):
         if self.get_cvar("qlx_brandingPrependMapName", bool):
@@ -45,5 +60,46 @@ class branding(minqlx.Plugin):
             cs += " - "
         minqlx.set_configstring(679, cs + (self.get_cvar("qlx_serverBrandBottomField")))
 
+        if self.get_cvar("qlx_rainbowBrandName", bool):
+            # Thanks Mino for this bit!
+            def rotating_colors():
+                i = 0
+                while True:
+                    res = (i % 7) + 1
+                    i += 1
+                    yield res
+
+            map_name = minqlx.get_configstring(3)
+            r = rotating_colors()
+            res = ""
+            for i in range(len(map_name)):
+                res += "^{}{}".format(next(r), map_name[i])
+
+            minqlx.set_configstring(3, res)
+
+    def player_connect(self, player):
+        if self.get_cvar("qlx_connectMessage") != None:
+            if player not in self.playerConnectedYetList:
+                self.playerConnectedYetList.append(player)
+                return "{}\nThis server is running ^4branding.py^7. ^2http://github.com/tjone270/Quake-Live^7.\n".format(self.get_cvar("qlx_connectMessage"))
+        
+    def player_loaded(self, player):
+        if self.get_cvar("qlx_loadedMessage") != None:
+            self.center_print(self.get_cvar("qlx_loadedMessage"))
+
+        try:
+            self.playerConnectedYetList.remove(player)
+        except:
+            return
+
+    def game_countdown(self):
+        if self.get_cvar("qlx_countdownMessage") != None:
+            self.center_print(self.get_cvar("qlx_countdownMessage"))
+
+    def game_end(self, data):
+        if self.get_cvar("qlx_endOfGameMessage") != None:
+            self.msg(self.get_cvar("qlx_endOfGameMessage"))
+
+            
     def cmd_showversion(self, player, msg, channel):
         channel.reply("^4branding.py^7 - version {}, created by Thomas Jones on 06/11/2015.".format(self.plugin_version))
