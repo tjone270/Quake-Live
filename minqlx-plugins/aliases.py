4a09c5f258a3219ac70e66e11b4e4fcda05f0968
Thomas Jones <thomas@tomtecsolutions.com>
2016-05-04 13:28:24 +1000

    update with saucy new features!

--- minqlx-plugins/aliases.py
+++ minqlx-plugins/aliases.py
@@ -14,24 +14,48 @@ class aliases(minqlx.Plugin):
         self.add_command("clearaliases", self.cmd_clearaliases, 5)
         self.add_command("tomtec_versions", self.cmd_showversion)
 
-        self.plugin_version = "1.6"
+        self.plugin_version = "1.7"
 
     def cmd_alias(self, player, msg, channel):
         if len(msg) < 2:
             return minqlx.RET_USAGE
-        
-        try:
-            steam_id = self.player(int(msg[1])).steam_id
-            player_name = self.player(int(msg[1])).name
-        except:
-            channel.reply("^1Invalid Client ID.^7 Enter a valid client ID to see a list of names they've used on this server.")
-            return
-        
-        key = "minqlx:players:{}".format(steam_id)
-        out = list(self.db.lrange(key, 0, -1))
-        channel.reply("{}^7 has played on this server under the following names:".format(player_name))
-        channel.reply(" ^4*^7  {}".format("   \n ^4*^7  ".join(out)))
 
+        try:
+            ident = int(msg[1])
+            target_player = None
+            if 0 <= ident < 64:
+                steam_id = self.player(int(msg[1])).steam_id
+                player_name = self.player(int(msg[1])).name
+                ip_address = self.player(int(msg[1])).ip
+            else:
+                player_name = ident
+                steam_id = ident
+                key = "minqlx:players:{}:ips".format(steam_id)
+                iplist = list(self.db.smembers(key))
+                ip_address = iplist[-1]
+        except ValueError:
+            channel.reply("Invalid ID. Use either a client ID or a SteamID64.")
+            return
+        except minqlx.NonexistentPlayerError:
+            channel.reply("Invalid client ID. Use either a client ID or a SteamID64.")
+            return
+
+        key = "minqlx:ips:{}".format(ip_address)
+        steamid_iplist = list(self.db.smembers(key))
+
+        namelist = list()
+        
+        for steam_id in steamid_iplist:
+            key = "minqlx:players:{}".format(steam_id)
+            namelist = namelist + (list(self.db.lrange(key, 0, -1)))
+            
+        channel.reply("{}^7 has played on this server under the following names:".format(player_name))
+        channel.reply(" ^4*^7  {}".format("   \n ^4*^7  ".join(namelist)))
+
+        channel.reply("{}^7 has played on this server under the following Steam64 IDs:".format(player_name))
+        channel.reply(" ^4*^7  {}".format("   \n ^4*^7  ".join(steamid_iplist)))
+
+        
     def cmd_clearaliases(self, player, msg, channel):
         # this function came from namesfix.py by Mino
         players = self.db.smembers("minqlx:players")
